968769ba2e17cded922ca2cbc6b37597f64603a5
121909 <121909@users.noreply.github.com>
2026-09-16 21:47:31 +0800

    v2.2.0: add KernelSU WebUI (webroot) for two auto-separation toggles

--- zte-charge-separate/customize.sh
+++ zte-charge-separate/customize.sh
@@ -6,7 +6,7 @@ ui_print "***************************************"
 ui_print "仅写 charge_separation_switch（1=分离，0=充电）"
 ui_print "插入时按类型与开关动作一次，不做持续监控"
 ui_print "断电后系统自动关闭分离，无需其它操作"
-ui_print "重启后点模块「执行/Action」可调整两个开关"
+ui_print "在 KernelSU 模块页打开 WebUI 可随时调整两个开关"
 
 set_perm_recursive "$MODPATH" 0 0 0755 0755
 set_perm "$MODPATH/service.sh" 0 0 0755
